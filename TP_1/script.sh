323cbad04402ceef67d5ee98f1d9424f06c44884
Agustin Zarate <azarate@teknedatalabs.com>
2025-03-19 18:42:39 -0300

    fin tp

--- TP_1/script.sh
+++ TP_1/script.sh
@@ -14,6 +14,7 @@ if [[ $1 == "-d" ]]; then
         #  501 34544     1   0 10:34PM ttys025    0:00.01 bash /Users/agustinzarate/EPNro1/consolidar.sh
     PID=$(ps -ef | grep "consolidar.sh" | grep -v grep | awk '{print $2}')
     
+    # -n chequea que no sea vacio
     if [ -n "$PID" ]; then
         kill $PID
         echo "Proceso con PID $PID detenido."
@@ -56,6 +57,7 @@ while true; do
     for file in ~/EPNro1/entrada/*.txt; do
         [[ -e "$file" ]] || continue
         echo "Processing $file..."
+        echo "" >> ~/EPNro1/salida/${FILENAME}.txt
         cat "$file" >> ~/EPNro1/salida/${FILENAME}.txt
         mv "$file" ~/EPNro1/procesado/
     done
@@ -74,9 +76,11 @@ FIN
             
             ;;
         3)
-            #.f para verificar si el archivo existe
+            #-f para verificar si el archivo existe
             if [[ -f ~/EPNro1/salida/${FILENAME}.txt ]]; then
                 echo "Listando alumnos ordenados por padrón..."
+                #-n indica que se ordena por numero
+                #-k 1 indica que se ordena por la columna 1
                 sort -n -k 1 ~/EPNro1/salida/${FILENAME}.txt
             else
                 echo "El archivo ${FILENAME}.txt no existe en salida."
@@ -88,18 +92,22 @@ FIN
 
                 #k 5 para ordenar por la columna 5
                 #n para ordenar numéricamente
-                #r para ordenar en orden inverso
-                #head -n 10 para mostrar solo las primeras 10 líneas
-                sort -k 5 -nr ~/EPNro1/salida/${FILENAME}.txt | head -n 10
+                #head para mostrar solo las primeras 10 líneas
+                #awk '{print $(NF), $0}' ~/EPNro1/salida/${FILENAME}.txt | sort -nr -k1,1 | cut -d' ' -f2- | head
+                sort -nr -k 5 ~/EPNro1/salida/${FILENAME}.txt | head
             else
                 echo "El archivo ${FILENAME}.txt no existe en salida."
             fi
             ;;
         5)
+            #read con mensaje
             read -p "Ingrese Nro_Padrón: " padron
+
             if [[ -f ~/EPNro1/salida/${FILENAME}.txt ]]; then
+
                 echo "Buscando Nro_Padrón $padron..."
-                grep "^$padron " ~/EPNro1/salida/${FILENAME}.txt || echo "No se encontraron datos para $padron."
+                
+                grep "$padron " ~/EPNro1/salida/${FILENAME}.txt || echo "No se encontraron datos para $padron."
             else
                 echo "El archivo ${FILENAME}.txt no existe en salida."
             fi
